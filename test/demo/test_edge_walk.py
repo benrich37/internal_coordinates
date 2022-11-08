16a25d5fff844d589aa80f49a09d24ed1f7aa992
benrich37 <76569522+benrich37@users.noreply.github.com>
2022-11-08 14:17:48 -0700

    test if same with shift work

--- test/demo/test_edge_walk.py
+++ test/demo/test_edge_walk.py
@@ -6,7 +6,11 @@ mainpath = os.path.join(os.path.dirname(__file__), '../../')
 sys.path.append(mainpath)
 from funcs import wilson, ic_gen, misc
 
-proponal = read('../toy_data/proponal.xyz')
-bonds_ref = misc.bonds_dict(proponal)
-edges = misc.get_edges(bonds_ref)
-print(edges)
+# proponal = read('../toy_data/proponal.xyz')
+# bonds_ref = misc.bonds_dict(proponal)
+# edges = misc.get_edges(bonds_ref)
+# print(edges)
+
+print(misc.same_edge([0,1,2], [0,1,3]))
+print(misc.same_edge([0,1,2], [0,1,2]))
+print(misc.same_edge([0,1,2], [1,0,2]))
