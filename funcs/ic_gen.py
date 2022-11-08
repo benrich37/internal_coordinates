3506ceafdefc71ab134a61592b428e8cefdc72bf
benrich37 <76569522+benrich37@users.noreply.github.com>
2022-11-08 11:59:08 -0700

    some refactoring

--- funcs/ic_gen.py
+++ funcs/ic_gen.py
@@ -2,25 +2,25 @@ from funcs import wilson
 from funcs import misc
 import numpy as np
 
-def get_n_string(atom_list, i, m):
+def get_n_pairs(atom_list, i, m):
     output = []
     for j in range(m):
         output.append(atom_list[i+j])
     return tuple(output)
 
-def get_n_strings(atom_list, n):
+def get_n_pair(atom_list, n):
     output = []
     # number of times we can fit an n_string into our atom list
     m = len(atom_list) + 1 - n
     for i in range(m):
-        output.append(get_n_string(atom_list, i, n))
+        output.append(get_n_pairs(atom_list, i, n))
     return output
 # naive approach
 def get_ic_idcs(atom_list):
     n = len(atom_list)
-    bonds = get_n_strings(atom_list, 2)
-    angles = get_n_strings(atom_list, 3)
-    dihedrals = get_n_strings(atom_list, 4)
+    bonds = get_n_pair(atom_list, 2)
+    angles = get_n_pair(atom_list, 3)
+    dihedrals = get_n_pair(atom_list, 4)
     return bonds, angles, dihedrals
 
 def get_ic_vecs(posns):
--- test/demo/projectout.py
+++ test/demo/projectout.py
@@ -4,9 +4,11 @@ import os
 import sys
 mainpath = os.path.join(os.path.dirname(__file__), '../../')
 sys.path.append(mainpath)
-from funcs import wilson, ic_gen
+from funcs import wilson, ic_gen, misc
 
 proponal = read('../toy_data/proponal.xyz')
 proponal2 = read('../toy_data/proponal2.xyz')
 
-disp_vec = proponal2.get_positions() - proponal.get_positions()
+#disp_vec = proponal2.get_positions() - proponal.get_positions()
+bonds_dict = misc.bonds_dict(proponal)
+print(bonds_dict)
